4d394b001a76772ff177e5b60bbe307b29844d78
safatsaat2 <sakibsafat47@gmail.com>
2023-04-08 01:48:39 +0600

    styling done of cart

--- src/components/ReviewItems/ReviewItems.jsx
+++ src/components/ReviewItems/ReviewItems.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import './ReviewItems.css'
+import './ReviewItems.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
 const ReviewItems = (product) => {
-    const {name, price, id, img, quantity} = product.product
+    const { name, price, id, img, quantity } = product.product
     return (
         <div className='div'>
             <img className='img' src={img} alt="" />
-            <h1>{name}</h1>
-            
-            
+            <div className='product-details'>
+                <p className='product-title'>{name}</p>
+                <p>Price: <span className='orange-color'>${price}</span></p>
+                <p>Quantity: <span className='orange-color'>${quantity}</span></p>
+            </div>
+            <button className='btn-delete'>
+                <FontAwesomeIcon icon={faTrashAlt}></FontAwesomeIcon>
+            </button>
         </div>
     );
 };
